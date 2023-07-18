88f28005caa618fab1c467cae05b0c17c621436f
DRK-V <flv3dsc@gmail.com>
2023-07-18 14:12:21 -0500

    va el navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-
-
+import { Navbar } from './page/navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-<h1>fdf</h1>
+    <Navbar/>
+    
     </>
   )
 }
