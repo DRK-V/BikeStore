4b9f967db5fe6c077d7d0269690eceaf2b4e4757
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2023-07-26 19:57:20 -0500

    fix main branch

--- bike_backend/routes/dataRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dataController = require('../controllers/dataController')
-
-router.get('/products',dataController.getProducts); // el primer servicio get (simple)
-
-module.exports = router;
