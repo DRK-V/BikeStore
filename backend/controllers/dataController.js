1d68a4d93956f66b7cf4a7a1c1734da1f4423051
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2023-07-28 10:21:43 -0500

    inserting fields in the products table

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -5,7 +5,7 @@ const pool = new Pool(CONFIG_BD);
 console.log(pool)
 
 const getProducts = (req, res) => {
-    pool.query('SELECT * FROM productos', (error, result) => {
+    pool.query('SELECT * FROM producto', (error, result) => {
         if (error) {
             // Imprimir el mensaje de error en la consola
             console.error('Error al obtener datos', error.message);
