c32a993ef0c5a05ced8e2d2f134ec882ddb08392
Stiven Colorado <90488682+StivenColorado@users.noreply.github.com>
2023-08-02 21:07:04 -0500

    fix imagen_producto table

--- backend/controllers/dataController.js
+++ backend/controllers/dataController.js
@@ -1,7 +1,7 @@
 const { pool } = require('../config/db');
 
 const getImages = (req, res) => {
-  pool.query('SELECT * FROM images', (error, result) => {
+  pool.query('SELECT * FROM imagen_producto', (error, result) => {
     if (error) {
       console.error('Error al obtener datos', error.message);
       res.status(500).json({ error: 'Error al obtener datos' });
