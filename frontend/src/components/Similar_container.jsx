05310afb5e85ccf97df8cb3861be0ecdc6c1df63
DRK-V <flv3dsc@gmail.com>
2023-08-17 13:48:03 -0500

    correccion de carrito

--- frontend/src/components/Similar_container.jsx
+++ frontend/src/components/Similar_container.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-import '../css/similar_bike.css'
-import { Card_container } from '../components/Card_container'
+// Similar_container.jsx
+import React from 'react';
+import '../css/similar_bike.css';
+import { Card_container } from './Card_container';
+
 const Similar_container = () => {
     return (
         <div className='slider_similar_bike'>
             <h1>Productos similares</h1>
-            <Card_container is_categories="similar">
-
-            </Card_container>
+            <Card_container is_categories="false" is_similar="true" />
         </div>
-    )
-}
-export default Similar_container
+    );
+};
+
+export default Similar_container;
