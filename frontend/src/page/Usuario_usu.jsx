107b070a3be4adf91914fc15793e9d9f7ecbb197
DRK-V <flv3dsc@gmail.com>
2023-08-24 14:09:08 -0500

    terminado de configuracion de usuario

--- frontend/src/page/Usuario_usu.jsx
+++ frontend/src/page/Usuario_usu.jsx
@@ -7,7 +7,7 @@ export const Usuario_usu = () => {
       <div className="pa_usu">
         <div className="usu_ini"></div>
         <div className="usu_detall">
-            <div className="titulo1.1">
+            <div className="titulo1">
           <h2>Mi perfil</h2>
             </div>
           <form action="" className="formulario_usu">
@@ -15,31 +15,31 @@ export const Usuario_usu = () => {
               <div className="fila1">
                 <div className="fila">
                   <label htmlFor=""><b>Nombre</b></label>
-                  <input className="input1" type="text" />
+                  <input className="input1" placeholder="Nombre" type="text" />
                 </div>
 
                 <div className="fila">
                   <label htmlFor=""><b>Correo</b></label>
-                  <input className="input1" type="text" />
+                  <input className="input1" placeholder="Correo" type="text" />
                 </div>
               </div>
 
               <div className="fila1">
                 <div className="fila">
                   <label htmlFor=""><b>Numero de Documento</b></label>
-                  <input className="input1" type="text" />
+                  <input className="input1" placeholder="Numero de Documento" type="text" />
                 </div>
 
                 <div className="fila">
                   <label htmlFor=""><b>Ciudad</b></label>
-                  <input className="input1" type="text" />
+                  <input className="input1" placeholder="Ciudad" type="text" />
                 </div>
               </div>
 
               <div className="fila1">
                 <div className="fila">
                   <label htmlFor=""><b>Telefono</b></label>
-                  <input className="input1" type="text" />
+                  <input className="input2" placeholder="Telefono" type="text" />
                 </div>
               </div>
             </div>
