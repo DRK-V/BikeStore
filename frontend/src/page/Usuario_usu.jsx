c913bec4533073f8b7baeb1fed4eaa6ca14b11c3
DRK-V <flv3dsc@gmail.com>
2023-08-24 17:06:02 -0500

    config user - contenedores de opciones

--- frontend/src/page/Usuario_usu.jsx
+++ frontend/src/page/Usuario_usu.jsx
@@ -1,8 +1,8 @@
 import { Footer } from "../components/Footer";
 import "../css/Usuario_config.css";
 import "../css/menu_profile.css";
-import { Menu_profile } from "../components/Menu_profile"
-import { FiX} from 'react-icons/fi';
+import { Menu_profile } from "../components/Menu_profile";
+import { FiX } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 export const Usuario_usu = () => {
@@ -10,48 +10,167 @@ export const Usuario_usu = () => {
     <>
       <div className="pa_usu">
         <div className="usu_ini">
-          <Menu_profile is_link_active={true}/>
+          <Menu_profile is_link_active={true} />
         </div>
         <div className="usu_detall">
-          <div className="titulo1">
-            <h2>Mi perfil</h2>
-           
-            <button className="exit_button">  <Link className="exit_image" to="/"><FiX className="exit_image" /></Link> </button>
-          </div>
-          <form action="" className="formulario_usu">
-            <div className="formu_usu">
-              <div className="fila1">
-                <div className="fila">
-                  <label htmlFor=""><b>Nombre</b></label>
-                  <input className="input1" placeholder="Nombre" type="text" />
-                </div>
+          <div className="container_my_usu">
+            <div className="titulo1">
+              <h2>Mi perfil</h2>
 
-                <div className="fila">
-                  <label htmlFor=""><b>Correo</b></label>
-                  <input className="input1" placeholder="Correo" type="text" />
-                </div>
-              </div>
-
-              <div className="fila1">
-                <div className="fila">
-                  <label htmlFor=""><b>Numero de Documento</b></label>
-                  <input className="input1" placeholder="Numero de Documento" type="text" />
-                </div>
-
-                <div className="fila">
-                  <label htmlFor=""><b>Ciudad</b></label>
-                  <input className="input1" placeholder="Ciudad" type="text" />
-                </div>
-              </div>
-
-              <div className="fila1">
-                <div className="fila">
-                  <label htmlFor=""><b>Telefono</b></label>
-                  <input className="input2" placeholder="Telefono" type="text" />
-                </div>
-              </div>
+              <button className="exit_button">
+                {" "}
+                <Link className="exit_image" to="/">
+                  <FiX className="exit_image" />
+                </Link>{" "}
+              </button>
             </div>
-          </form>
+            <form action="" className="formulario_usu">
+              <div className="formu_usu">
+                <div className="fila1">
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Nombre</b>
+                    </label>
+                    <input
+                      disabled
+                      className="input1"
+                      placeholder="Nombre"
+                      type="text"
+                    />
+                  </div>
+
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Correo</b>
+                    </label>
+                    <input
+                      disabled
+                      className="input1"
+                      placeholder="Correo"
+                      type="text"
+                    />
+                  </div>
+                </div>
+
+                <div className="fila1">
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Numero de Documento</b>
+                    </label>
+                    <input
+                      disabled
+                      className="input1"
+                      placeholder="Numero de Documento"
+                      type="text"
+                    />
+                  </div>
+
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Ciudad</b>
+                    </label>
+                    <input
+                      disabled
+                      className="input1"
+                      placeholder="Ciudad"
+                      type="text"
+                    />
+                  </div>
+                </div>
+
+                <div className="fila1">
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Telefono</b>
+                    </label>
+                    <input
+                      disabled
+                      className="input2"
+                      placeholder="Telefono"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="container_my_config">
+            <div className="titulo1">
+              <h2>Mi perfil</h2>
+
+              <button className="exit_button">
+                {" "}
+                <Link className="exit_image" to="/">
+                  <FiX className="exit_image" />
+                </Link>{" "}
+              </button>
+            </div>
+            <form action="" className="formulario_usu">
+              <div className="formu_usu">
+                <div className="fila1">
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Nombre</b>
+                    </label>
+                    <input
+                      className="input1"
+                      placeholder="Nombre"
+                      type="text"
+                    />
+                  </div>
+
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Correo</b>
+                    </label>
+                    <input
+                      className="input1"
+                      placeholder="Correo"
+                      type="text"
+                    />
+                  </div>
+                </div>
+
+                <div className="fila1">
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Numero de Documento</b>
+                    </label>
+                    <input
+                      className="input1"
+                      placeholder="Numero de Documento"
+                      type="text"
+                    />
+                  </div>
+
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Ciudad</b>
+                    </label>
+                    <input
+                      className="input1"
+                      placeholder="Ciudad"
+                      type="text"
+                    />
+                  </div>
+                </div>
+
+                <div className="fila1">
+                  <div className="fila">
+                    <label htmlFor="">
+                      <b>Telefono</b>
+                    </label>
+                    <input
+                      className="input2"
+                      placeholder="Telefono"
+                      type="text"
+                    />
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="container_my_order"></div>
         </div>
       </div>
 
