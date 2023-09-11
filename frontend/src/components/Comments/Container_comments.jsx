6bab27add35f1bdfeb97b86af8c0bf0d113c4ce7
DRK-V <flv3dsc@gmail.com>
2023-09-11 15:34:12 -0500

    b

--- frontend/src/components/Comments/Container_comments.jsx
+++ frontend/src/components/Comments/Container_comments.jsx
@@ -1,66 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import '../../css/Comments/Comments.css';
-import Comments from './Comments';
-import { useComenContext } from '../comencontex'; 
-import { useAuth } from '../AuthContext';
+import React, { useState, useEffect } from "react";
+import "../../css/Comments/Comments.css";
+import Comments from "./Comments";
+import { useComenContext } from "../comencontex";
+import { useAuth } from "../AuthContext";
 
 const Container_comments = () => {
   const [commentsData, setCommentsData] = useState([]);
-  const [newComment, setNewComment] = useState('');
+  const [newComment, setNewComment] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const comenContext = useComenContext(); 
-  const authContext = useAuth(); 
+  const comenContext = useComenContext();
+  const authContext = useAuth();
+  const [isCommentValid, setIsCommentValid] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
 
     const fetchComments = async () => {
       try {
-       
         const codigo_producto = comenContext.selectedProductId;
 
         if (!codigo_producto) {
-          console.error('El código del producto no es válido');
+          console.error("El código del producto no es válido");
           return;
         }
 
-       
-        const response = await fetch(`http://localhost:3060/coments/${codigo_producto}`);
+        const response = await fetch(
+          `http://localhost:3060/coments/${codigo_producto}`
+        );
         if (!response.ok) {
-          throw new Error('No se pudieron cargar los comentarios');
+          throw new Error("No se pudieron cargar los comentarios");
         }
 
         const data = await response.json();
-        console.log('Comentarios obtenidos:', data);
+        console.log("Comentarios obtenidos:", data);
 
         const promises = data.map((comment) => {
           const codigo_cliente = comment.codigo_cliente;
           return fetch(`http://localhost:3060/cliente/${codigo_cliente}`)
             .then((response) => response.json())
             .then((userData) => {
-              console.log(`Nombre de usuario para el cliente ${codigo_cliente}:`, userData.nombre_usuario);
+              console.log(
+                `Nombre de usuario para el cliente ${codigo_cliente}:`,
+                userData.nombre_usuario
+              );
               return {
                 ...comment,
-                clientName: userData.nombre_usuario || 'Nombre no encontrado',
+                clientName: userData.nombre_usuario || "Nombre no encontrado",
               };
             })
             .catch((error) => {
-              console.error(`Error al obtener el nombre de usuario para el comentario ${comment.id}:`, error);
+              console.error(
+                `Error al obtener el nombre de usuario para el comentario ${comment.id}:`,
+                error
+              );
               return comment;
             });
         });
 
-     
         const commentsWithNames = await Promise.all(promises);
 
-        console.log('Comentarios con nombres de usuario:', commentsWithNames);
+        console.log("Comentarios con nombres de usuario:", commentsWithNames);
         setCommentsData(commentsWithNames);
         setIsLoading(false);
       } catch (error) {
         setError(error);
         setIsLoading(false);
-        console.error('Error al cargar comentarios:', error);
+        console.error("Error al cargar comentarios:", error);
       }
     };
 
@@ -68,30 +74,34 @@ const Container_comments = () => {
   }, [comenContext.selectedProductId]);
 
   const handleCommentChange = (e) => {
-    setNewComment(e.target.value);
+    const comment = e.target.value;
+    setNewComment(comment);
+
+    if (comment.length >= 20) {
+      setIsCommentValid(true);
+    } else {
+      setIsCommentValid(false);
+    }
   };
 
   const submitComment = () => {
-   
     if (!authContext.isLoggedIn) {
-      console.error('El usuario no está autenticado');
+      console.error("El usuario no está autenticado");
       return;
     }
 
-    const codigo_cliente = authContext.user.id_cliente; 
-    const codigo_producto = comenContext.selectedProductId; 
+    const codigo_cliente = authContext.user.id_cliente;
+    const codigo_producto = comenContext.selectedProductId;
 
-    
     if (!codigo_producto) {
-      console.error('El código del producto no es válido');
+      console.error("El código del producto no es válido");
       return;
     }
 
-    
-    fetch('http://localhost:3060/comentarios', {
-      method: 'POST',
+    fetch("http://localhost:3060/comentarios", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         codigo_cliente,
@@ -101,34 +111,33 @@ const Container_comments = () => {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log('Comentario enviado exitosamente:', data);
+        console.log("Comentario enviado exitosamente:", data);
 
-        
         const commentWithUserName = {
           codigo_cliente,
-          clientName: authContext.user.nombre_usuario || 'Nombre no encontrado',
+          clientName: authContext.user.nombre_usuario || "Nombre no encontrado",
           fecha_creacion: new Date().toISOString(),
           texto: newComment,
         };
 
         setCommentsData([...commentsData, commentWithUserName]);
-        setNewComment('');
+        setNewComment("");
       })
       .catch((error) => {
-        console.error('Error al enviar el comentario:', error);
+        console.error("Error al enviar el comentario:", error);
       });
   };
 
   const formatDateTime = (dateTimeString) => {
     const date = new Date(dateTimeString);
-    const day = date.getDate().toString().padStart(2, '0');
-    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
     const year = date.getFullYear();
     return `${day}/${month}/${year}`;
   };
 
   return (
-    <div className='Comments'>
+    <div className="Comments">
       <h1 className="title_comments">Comentarios</h1>
       {isLoading ? (
         <p>Cargando comentarios...</p>
@@ -144,19 +153,26 @@ const Container_comments = () => {
               content={comment.texto}
             />
           ))}
-          {authContext.isLoggedIn && ( 
-            <form className='for_coment' onSubmit={(e) => {
-              e.preventDefault();
-              submitComment();
-            }}>
+          {authContext.isLoggedIn && (
+            <form
+              className="for_coment"
+              onSubmit={(e) => {
+                e.preventDefault();
+                submitComment();
+              }}
+            >
               <input
-                className='coments'
+                className="coments"
                 type="text"
                 placeholder="Escribe tu comentario"
                 value={newComment}
                 onChange={handleCommentChange}
               />
-              <button className='coments_buton' type="submit">
+              <button
+                className="coments_buton"
+                type="submit"
+                disabled={!isCommentValid}
+              >
                 <span class="material-symbols-outlined">send</span>
               </button>
             </form>
--- frontend/src/page/Payment.jsx
+++ frontend/src/page/Payment.jsx
@@ -1,182 +1,194 @@
 //payment
-import React, { useState } from 'react';
-import { Navbar } from '../components/Navbar';
-import '../css/cart_shopping.css';
-import { Footer } from '../components/Footer';
+import React, { useState } from "react";
+import { Navbar } from "../components/Navbar";
+import "../css/cart_shopping.css";
+import { Footer } from "../components/Footer";
 import { useAuth } from "../components/AuthContext";
-
+import { useLocation } from "react-router-dom";
 export const Payment = () => {
-    const { user } = useAuth();
-    // Estado para almacenar los valores de los campos
-    const [formValues, setFormValues] = useState({
-        nombreTitular: user.nombre_usuario || '', // Rellena con el nombre de usuario
-        tipoDocumento: user.tipo_de_documento || '', // Rellena con el tipo de documento
-        numeroDocumento: user.numero_de_documento || '', // Rellena con el número de documento
-        correoElectronico: user.correo|| '', // Rellena con el correo electrónico
-        confirmacionCorreo: user.correo,
-        valorPagar: '',
-      });
+  const location = useLocation();
+  const { user } = useAuth();
+  // Estado para almacenar los valores de los campos
+  const [formValues, setFormValues] = useState({
+    nombreTitular: user.nombre_usuario || "",
+    tipoDocumento: user.tipo_de_documento || "",
+    numeroDocumento: user.numero_de_documento || "",
+    correoElectronico: user.correo || "",
+    confirmacionCorreo: user.correo,
+    valorPagar: location.state ? location.state.valorPagar : "", // Obtiene el valorPagar del estado pasado en la URL
+  });
+  // Función para manejar los cambios en los campos del formulario
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
 
-    // Función para manejar los cambios en los campos del formulario
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormValues({
-            ...formValues,
-            [name]: value,
-        });
-    };
+  // Función para manejar el envío del formulario
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Aquí puedes enviar los datos del formulario a través de una solicitud HTTP o realizar cualquier otra acción necesaria.
+    console.log("Formulario enviado:", formValues);
+  };
 
-    // Función para manejar el envío del formulario
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        // Aquí puedes enviar los datos del formulario a través de una solicitud HTTP o realizar cualquier otra acción necesaria.
-        console.log('Formulario enviado:', formValues);
-    };
+  return (
+    <>
+      <Navbar />
+      <form className="payment-form" onSubmit={handleSubmit}>
+        <div className="input-container">
+          <label htmlFor="nombreTitular">
+            Nombre del titular de la cuenta:
+          </label>
+          <div className="input-icon-container">
+            <i className="fas fa-user"></i> {/* Icono de nombre */}
+            <input
+              type="text"
+              id="nombreTitular"
+              name="nombreTitular"
+              value={formValues.nombreTitular}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-    return (
-        <>
-            <Navbar />
-                <form className="payment-form" onSubmit={handleSubmit}>
-                    <div className="input-container">
-                        <label htmlFor="nombreTitular">Nombre del titular de la cuenta:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-user"></i> {/* Icono de nombre */}
-                            <input
-                                type="text"
-                                id="nombreTitular"
-                                name="nombreTitular"
-                                value={formValues.nombreTitular}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="tipoDocumento">Tipo de documento:</label>
+          <div className="input-icon-container">
+            <i className="fas fa-id-card"></i>{" "}
+            {/* Icono de tipo de documento */}
+            <input
+              type="text"
+              id="tipoDocumento"
+              name="tipoDocumento"
+              value={formValues.tipoDocumento}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="tipoDocumento">Tipo de documento:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-id-card"></i> {/* Icono de tipo de documento */}
-                            <input
-                                type="text"
-                                id="tipoDocumento"
-                                name="tipoDocumento"
-                                value={formValues.tipoDocumento}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="numeroDocumento">Número de documento:</label>
+          <div className="input-icon-container">
+            <i className="fas fa-address-card"></i>{" "}
+            {/* Icono de número de documento */}
+            <input
+              type="text"
+              id="numeroDocumento"
+              name="numeroDocumento"
+              value={formValues.numeroDocumento}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="numeroDocumento">Número de documento:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-address-card"></i> {/* Icono de número de documento */}
-                            <input
-                                type="text"
-                                id="numeroDocumento"
-                                name="numeroDocumento"
-                                value={formValues.numeroDocumento}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="tipoCuenta">Tipo de cuenta:</label>
+          <div className="input-icon-container">
+            <i className="fas fa-university"></i>{" "}
+            {/* Icono de tipo de cuenta */}
+            <input
+              type="text"
+              id="tipoCuenta"
+              name="tipoCuenta"
+              value={formValues.tipoCuenta}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="tipoCuenta">Tipo de cuenta:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-university"></i> {/* Icono de tipo de cuenta */}
-                            <input
-                                type="text"
-                                id="tipoCuenta"
-                                name="tipoCuenta"
-                                value={formValues.tipoCuenta}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="banco">Banco:</label>
+          <div className="input-icon-container">
+            <i className="fas fa-piggy-bank"></i> {/* Icono de banco */}
+            <input
+              type="text"
+              id="banco"
+              name="banco"
+              value={formValues.banco}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="banco">Banco:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-piggy-bank"></i> {/* Icono de banco */}
-                            <input
-                                type="text"
-                                id="banco"
-                                name="banco"
-                                value={formValues.banco}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="numeroCuenta">Número de cuenta:</label>
+          <div className="input-icon-container">
+            <i className="fas fa-credit-card"></i>{" "}
+            {/* Icono de número de cuenta */}
+            <input
+              type="text"
+              id="numeroCuenta"
+              name="numeroCuenta"
+              value={formValues.numeroCuenta}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="numeroCuenta">Número de cuenta:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-credit-card"></i> {/* Icono de número de cuenta */}
-                            <input
-                                type="text"
-                                id="numeroCuenta"
-                                name="numeroCuenta"
-                                value={formValues.numeroCuenta}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="correoElectronico">Correo electrónico:</label>
+          <div className="input-icon-container">
+            <i className="fas fa-envelope"></i>{" "}
+            {/* Icono de correo electrónico */}
+            <input
+              type="email"
+              id="correoElectronico"
+              name="correoElectronico"
+              value={formValues.correoElectronico}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="correoElectronico">Correo electrónico:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-envelope"></i> {/* Icono de correo electrónico */}
-                            <input
-                                type="email"
-                                id="correoElectronico"
-                                name="correoElectronico"
-                                value={formValues.correoElectronico}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="confirmacionCorreo">
+            Confirmación de correo electrónico:
+          </label>
+          <div className="input-icon-container">
+            <i className="fas fa-envelope"></i>{" "}
+            {/* Icono de correo electrónico */}
+            <input
+              type="email"
+              id="confirmacionCorreo"
+              name="confirmacionCorreo"
+              value={formValues.confirmacionCorreo}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="confirmacionCorreo">Confirmación de correo electrónico:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-envelope"></i> {/* Icono de correo electrónico */}
-                            <input
-                                type="email"
-                                id="confirmacionCorreo"
-                                name="confirmacionCorreo"
-                                value={formValues.confirmacionCorreo}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
+        <div className="input-container">
+          <label htmlFor="valorPagar">Valor a pagar:</label>
+          <div className="input-icon-container">
+            <i className="fas fa-dollar-sign"></i>{" "}
+            {/* Icono de valor a pagar */}
+            <input
+              disabled  
+              type="number"
+              id="valorPagar"
+              name="valorPagar"
+              value={formValues.valorPagar}
+              onChange={handleChange}
+              required
+            />
+          </div>
+        </div>
 
-                    <div className="input-container">
-                        <label htmlFor="valorPagar">Valor a pagar:</label>
-                        <div className="input-icon-container">
-                            <i className="fas fa-dollar-sign"></i> {/* Icono de valor a pagar */}
-                            <input
-                                type="number"
-                                id="valorPagar"
-                                name="valorPagar"
-                                value={formValues.valorPagar}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                    </div>
-
-                    <button type="submit">Enviar</button>
-                </form>
-            <Footer />
-        </>
-    );
+        <button type="submit">Enviar</button>
+      </form>
+      <Footer />
+    </>
+  );
 };
 
 export default Payment;
