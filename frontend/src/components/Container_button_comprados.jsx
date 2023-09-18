c47c8249da31522728f1d822fe3b2a39855d0930
Stiven <stivenchoo@gmail.com>
2023-09-18 12:14:03 -0500

    arreglando el responsive

--- frontend/src/components/Container_button_comprados.jsx
+++ frontend/src/components/Container_button_comprados.jsx
@@ -2,11 +2,8 @@ import '../css/button_comprados.css'
 export const Container_button_comprados = () => {
     return (
         <div className="container_button_comprados">
-            <button className="button_comprados">
-                <a className='button_comprados' href="/filters">
+            <button className="button_comprados" type='button'>
                     Mas comprados
-                </a>
-
             </button>
         </div>
 
